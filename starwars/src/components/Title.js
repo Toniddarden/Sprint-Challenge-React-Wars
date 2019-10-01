eecdfcc387485e641947e55e58c2d50122c8b150
Toniddarden1 <Toniddarden@gmail.com>
2019-09-30 21:57:35 -0400

    changed card color

--- starwars/src/components/Title.js
+++ starwars/src/components/Title.js
@@ -8,7 +8,7 @@ import { Card, CardImgOverlay, CardTitle, CardText } from 'reactstrap';
 
 const Title = (props) => {
 return (
-    <Card body inverse style={{ backgroundColor: 'blue', width: '200px', margin: '50px', padding: '20px' }}>
+    <Card body inverse style={{ backgroundColor: 'lightblue', width: '200px', margin: '50px', padding: '20px' }}>
 <CardImgOverlay top width="100%" src="/assets/256x186.svg" alt="Card image cap">
 <CardTitle>Name: {props.data.name}</CardTitle>
 <CardText>Gender: {props.data.gender}</CardText>
@@ -17,7 +17,7 @@ return (
 
     </Card>
 
-  
+
       
 )
 }
